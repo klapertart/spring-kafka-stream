493f1989438c51d27dde6999f35afc19643440e3
otong.sunandar <otong.sunandar@tritronik.com>
2023-03-03 14:24:23 +0700

    init commit

---
+++ src/main/java/klapertart/lab/kafkastream/BelajarKafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package klapertart.lab.kafkastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarKafkaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarKafkaStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/klapertart/lab/kafkastream/BelajarKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package klapertart.lab.kafkastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarKafkaStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
